fbc0ad3b28865d884236ecd25d9bdb9bb9643dc4
Sanarielsen <sanarielsen.dev@gmail.com>
2023-05-07 17:35:34 -0300

    feat(lts-07): add icon and logo in application
    add icon and logo in application (header and icon app) and remove unused
    images.

--- src/components/MenuHeader.tsx
+++ src/components/MenuHeader.tsx
@@ -1,4 +1,5 @@
-import { List, MicrophoneStage, UserCircle } from "phosphor-react";
+import Image from 'next/image'
+import { List, UserCircle } from "phosphor-react";
 
 interface MenuHeaderProps {
 
@@ -10,15 +11,15 @@ export default function MenuHeader( { onClickMenuControl }: MenuHeaderProps ) {
   return (
 
     <header className="menu-header flex justify-between bg-pachi-gray-header mb-4 py-5 px-5">      
-      <div className="menu-control">
+      <div className="menu-control  flex align-middle">
         <button className="first:hover:text-pachi-green" onClick={onClickMenuControl}>
           <List size={32} />
         </button>
       </div>
-      <div className="menu-brand"> 
-        <MicrophoneStage size={32} />
+      <div className="menu-brand flex align-middle"> 
+        <Image src="/logo-light.png" width={48} height={32} alt="Logo Letsing"/>
       </div>
-      <div className="menu-brand"> 
+      <div className="menu-brand flex align-middle items-center"> 
         <UserCircle size={32} />
       </div>
     </header>
